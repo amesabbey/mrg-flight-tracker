8c445fbe380ab6f738088666a2b54568236df058
amesabbey <amesabbey@gmail.com>
2024-07-17 18:50:57 -0700

    login popup success

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,30 +1,43 @@
 import { Component } from '@angular/core';
-import { GoogleAuthProvider, signInWithRedirect } from '@angular/fire/auth';
-import { getAuth } from "firebase/auth";
+import { CommonModule } from '@angular/common';
+import { Auth, GoogleAuthProvider, signInWithPopup, signInWithRedirect } from '@angular/fire/auth';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorComponent } from '../error/error.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
-  constructor(public dialog: MatDialog) {}
+  activeUser: boolean = false;
+
+  constructor(public auth: Auth, public router: Router, public dialog: MatDialog) {}
   
   loginWithGoogle() {
-    try {
-      signInWithRedirect(getAuth(), new GoogleAuthProvider());
-    } catch (error) {
+    signInWithPopup(this.auth, new GoogleAuthProvider())
+    .then((value) => {
+      console.log(value);
+      this.activeUser = true;
+      this.router.navigate(['/view-flights']);
+    })
+    .catch((error) => {
+      console.log(error);
       this.displayError(error);
-    }
+    });
+  }
+
+  logout() {
+    this.auth.signOut();
   }
 
   displayError(error: any) {
     this.dialog.open(ErrorComponent, { width: '500px', data: { message: error } });
   }
+
 }
